8e4da239a0a14e38e36cf369594bb9869d74c7b5
RReza131 <r.reza131@gmail.com>
2014-08-08 08:03:07 -0500

    Created MAR Adjustment Application
    Started an application to aid with moving MAR forms up or down for
    alignment.

---
+++ MarAdjustment/MarAdjustment.Designer.cs
@@ -0,0 +1,329 @@
+﻿namespace MarAdjustment
+{
+    partial class MarAdjustmentMainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPageHeight = new System.Windows.Forms.Label();
+            this.uxPageHeight = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.uxRowsPerDetail = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.uxDetailsPerPage = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.uxTopMarginOG = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.uxHeaderHeightOG = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.uxDetailHeightOG = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.uxPageFooterHeightOG = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.uxBottomMarginOG = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lblCommentTextSuggestions = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.uxHeaderHeightSug = new System.Windows.Forms.TextBox();
+            this.uxPageFooterHeightSug = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.uxDesiredMovement = new System.Windows.Forms.ComboBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblPageHeight
+            // 
+            this.lblPageHeight.AutoSize = true;
+            this.lblPageHeight.Location = new System.Drawing.Point(12, 12);
+            this.lblPageHeight.Name = "lblPageHeight";
+            this.lblPageHeight.Size = new System.Drawing.Size(69, 13);
+            this.lblPageHeight.TabIndex = 0;
+            this.lblPageHeight.Text = "Page Height:";
+            // 
+            // uxPageHeight
+            // 
+            this.uxPageHeight.Location = new System.Drawing.Point(116, 9);
+            this.uxPageHeight.Name = "uxPageHeight";
+            this.uxPageHeight.Size = new System.Drawing.Size(100, 20);
+            this.uxPageHeight.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Rows per Detail:";
+            // 
+            // uxRowsPerDetail
+            // 
+            this.uxRowsPerDetail.Location = new System.Drawing.Point(116, 35);
+            this.uxRowsPerDetail.Name = "uxRowsPerDetail";
+            this.uxRowsPerDetail.Size = new System.Drawing.Size(100, 20);
+            this.uxRowsPerDetail.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Details per Page:";
+            // 
+            // uxDetailsPerPage
+            // 
+            this.uxDetailsPerPage.Location = new System.Drawing.Point(116, 61);
+            this.uxDetailsPerPage.Name = "uxDetailsPerPage";
+            this.uxDetailsPerPage.Size = new System.Drawing.Size(100, 20);
+            this.uxDetailsPerPage.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Desired Movement:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 199);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Top Margin:";
+            // 
+            // uxTopMarginOG
+            // 
+            this.uxTopMarginOG.Location = new System.Drawing.Point(116, 192);
+            this.uxTopMarginOG.Name = "uxTopMarginOG";
+            this.uxTopMarginOG.Size = new System.Drawing.Size(100, 20);
+            this.uxTopMarginOG.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 223);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Header Height:";
+            // 
+            // uxHeaderHeightOG
+            // 
+            this.uxHeaderHeightOG.Location = new System.Drawing.Point(116, 220);
+            this.uxHeaderHeightOG.Name = "uxHeaderHeightOG";
+            this.uxHeaderHeightOG.Size = new System.Drawing.Size(100, 20);
+            this.uxHeaderHeightOG.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 249);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(71, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Detail Height:";
+            // 
+            // uxDetailHeightOG
+            // 
+            this.uxDetailHeightOG.Location = new System.Drawing.Point(116, 246);
+            this.uxDetailHeightOG.Name = "uxDetailHeightOG";
+            this.uxDetailHeightOG.Size = new System.Drawing.Size(100, 20);
+            this.uxDetailHeightOG.TabIndex = 9;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 275);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(74, 13);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Footer Height:";
+            // 
+            // uxPageFooterHeightOG
+            // 
+            this.uxPageFooterHeightOG.Location = new System.Drawing.Point(116, 272);
+            this.uxPageFooterHeightOG.Name = "uxPageFooterHeightOG";
+            this.uxPageFooterHeightOG.Size = new System.Drawing.Size(100, 20);
+            this.uxPageFooterHeightOG.TabIndex = 11;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 301);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(78, 13);
+            this.label9.TabIndex = 0;
+            this.label9.Text = "Bottom Margin:";
+            // 
+            // uxBottomMarginOG
+            // 
+            this.uxBottomMarginOG.Location = new System.Drawing.Point(116, 298);
+            this.uxBottomMarginOG.Name = "uxBottomMarginOG";
+            this.uxBottomMarginOG.Size = new System.Drawing.Size(100, 20);
+            this.uxBottomMarginOG.TabIndex = 13;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 396);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(122, 13);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "Suggestions/Comments:";
+            // 
+            // lblCommentTextSuggestions
+            // 
+            this.lblCommentTextSuggestions.AutoSize = true;
+            this.lblCommentTextSuggestions.Location = new System.Drawing.Point(12, 418);
+            this.lblCommentTextSuggestions.Name = "lblCommentTextSuggestions";
+            this.lblCommentTextSuggestions.Size = new System.Drawing.Size(75, 13);
+            this.lblCommentTextSuggestions.TabIndex = 0;
+            this.lblCommentTextSuggestions.Text = "Comment Text";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(144, 162);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(42, 13);
+            this.label11.TabIndex = 0;
+            this.label11.Text = "Original";
+            // 
+            // uxHeaderHeightSug
+            // 
+            this.uxHeaderHeightSug.Location = new System.Drawing.Point(222, 220);
+            this.uxHeaderHeightSug.Name = "uxHeaderHeightSug";
+            this.uxHeaderHeightSug.Size = new System.Drawing.Size(100, 20);
+            this.uxHeaderHeightSug.TabIndex = 8;
+            // 
+            // uxPageFooterHeightSug
+            // 
+            this.uxPageFooterHeightSug.Location = new System.Drawing.Point(222, 272);
+            this.uxPageFooterHeightSug.Name = "uxPageFooterHeightSug";
+            this.uxPageFooterHeightSug.Size = new System.Drawing.Size(100, 20);
+            this.uxPageFooterHeightSug.TabIndex = 12;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(243, 162);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(58, 13);
+            this.label12.TabIndex = 0;
+            this.label12.Text = "Suggested";
+            // 
+            // uxDesiredMovement
+            // 
+            this.uxDesiredMovement.FormattingEnabled = true;
+            this.uxDesiredMovement.Location = new System.Drawing.Point(117, 116);
+            this.uxDesiredMovement.Name = "uxDesiredMovement";
+            this.uxDesiredMovement.Size = new System.Drawing.Size(121, 21);
+            this.uxDesiredMovement.TabIndex = 4;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(13, 347);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(432, 25);
+            this.btnCalculate.TabIndex = 15;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // MarAdjustmentMainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(457, 542);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.uxDesiredMovement);
+            this.Controls.Add(this.uxBottomMarginOG);
+            this.Controls.Add(this.uxPageFooterHeightSug);
+            this.Controls.Add(this.uxPageFooterHeightOG);
+            this.Controls.Add(this.lblCommentTextSuggestions);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.uxDetailHeightOG);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.uxHeaderHeightSug);
+            this.Controls.Add(this.uxHeaderHeightOG);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.uxTopMarginOG);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.uxDetailsPerPage);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.uxRowsPerDetail);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.uxPageHeight);
+            this.Controls.Add(this.lblPageHeight);
+            this.Name = "MarAdjustmentMainForm";
+            this.Text = "Mar Adjustment";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPageHeight;
+        private System.Windows.Forms.TextBox uxPageHeight;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox uxRowsPerDetail;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox uxDetailsPerPage;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox uxTopMarginOG;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox uxHeaderHeightOG;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox uxDetailHeightOG;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox uxPageFooterHeightOG;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox uxBottomMarginOG;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lblCommentTextSuggestions;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox uxHeaderHeightSug;
+        private System.Windows.Forms.TextBox uxPageFooterHeightSug;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.ComboBox uxDesiredMovement;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
+
---
+++ MarAdjustment/MarAdjustment.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MarAdjustment
+{
+    public partial class MarAdjustmentMainForm : Form
+    {
+        #region Constructor
+        public MarAdjustmentMainForm()
+        {
+            InitializeComponent();
+            LoadLookupLists();
+        }
+        #endregion
+
+        #region Enums
+        public enum MovementType
+        {
+            Down = 0,
+            Up = 1      
+        }
+        #endregion
+
+        #region Properties and Fields
+
+        private decimal _headerHeightSug;
+        private decimal _footerHeighSug;
+        private decimal _headerHeightSug;
+
+        public decimal pageHeight { get; set; }
+
+        public int rowsPerDetail { get; set; }
+        public int detailsPerPage { get; set; }
+
+        public MovementType desiredMovement { get; set; }
+
+        public decimal topMarginOG { get; set; }
+        public decimal headerHeightOG { get; set; }
+        public decimal detailHeightOG { get; set; }
+        public decimal footerHeightOG { get; set; }
+        public decimal bottomMarginHeightOG { get; set; }
+
+        public decimal headerHeightSug        
+        {
+            get
+            {
+                return _headerHeightSug;
+            }
+
+            set
+            {
+                value = value <= 0 ? 0 : value;
+                _headerHeightSug = value;
+            }
+        }
+        public decimal footerHeightSug
+        {
+            get
+            {
+                return _footerHeighSug;
+            }
+
+            set
+            {
+                value = value <= 0 ? 0 : value;
+                _footerHeighSug = value;
+            }
+        }
+
+        private string suggestedComments { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        #region LookUpLists
+        private void LoadLookupLists()
+        {
+            LoadDesiredMovementList();
+        }
+
+        private void LoadDesiredMovementList()
+        {
+            Dictionary<MovementType, string> list = new Dictionary<MovementType, string>();
+            list.Add(MovementType.Down, "Down");
+            list.Add(MovementType.Up, "Up");
+
+            uxDesiredMovement.DisplayMember = "Value";
+            uxDesiredMovement.ValueMember = "Key";
+            uxDesiredMovement.DataSource = list.ToList();
+        }
+        #endregion
+
+        private void SetValues()
+        {
+             pageHeight = uxPageHeight.Text.Trim() != string.Empty? decimal.Parse(uxPageHeight.Text.Trim()):0;
+             rowsPerDetail = uxRowsPerDetail.Text.Trim() != string.Empty ? int.Parse(uxRowsPerDetail.Text.Trim()) : 0;
+             detailsPerPage = uxDetailsPerPage.Text.Trim() != string.Empty ? int.Parse(uxDetailsPerPage.Text.Trim()) : 0;
+
+             desiredMovement = (MovementType)uxDesiredMovement.SelectedValue;
+
+             topMarginOG = uxTopMarginOG.Text.Trim() != string.Empty? decimal.Parse(uxTopMarginOG.Text.Trim()):0;
+             headerHeightOG = uxHeaderHeightOG.Text.Trim() != string.Empty? decimal.Parse(uxHeaderHeightOG.Text.Trim()):0;
+             detailHeightOG = uxDetailHeightOG.Text.Trim() != string.Empty? decimal.Parse(uxDetailHeightOG.Text.Trim()):0;
+             footerHeightOG = uxPageFooterHeightOG.Text.Trim() != string.Empty ? decimal.Parse(uxPageFooterHeightOG.Text.Trim()) : 0;
+             bottomMarginHeightOG = uxBottomMarginOG.Text.Trim() != string.Empty ? decimal.Parse(uxBottomMarginOG.Text.Trim()) : 0;
+
+             //topMarginSug = uxTopMarginSug.Text.Trim() != string.Empty ? decimal.Parse(uxTopMarginSug.Text.Trim()) : 0;
+             //headerHeightSug = uxHeaderHeightSug.Text.Trim() != string.Empty ? decimal.Parse(uxHeaderHeightSug.Text.Trim()) : 0;
+             //detailHeightSug = uxDetailHeightSug.Text.Trim() != string.Empty ? decimal.Parse(uxDetailHeightSug.Text.Trim()) : 0;
+             //footerHeightSug = uxPageFooterHeightSug.Text.Trim() != string.Empty ? decimal.Parse(uxPageFooterHeightSug.Text.Trim()) : 0;
+             //bottomMarginHeightSug = uxBottomMarginSug.Text.Trim() != string.Empty ? decimal.Parse(uxBottomMarginSug.Text.Trim()) : 0; 
+        }
+
+        private string calculateHeaderHeight(decimal headerHeight, decimal detailHeight, MovementType Movement)
+        {
+            decimal calculatedHeight;
+
+            if (Movement == MovementType.Up)
+            {
+                calculatedHeight = headerHeight - detailHeight;
+            }
+            else
+            {
+                calculatedHeight = headerHeight + detailHeight;
+            }        
+            
+            return calculatedHeight.ToString("0.00");
+        }
+        private string calculateFooterHeight(decimal footerHeight, decimal detailHeight, MovementType Movement)
+        {
+            decimal calculatedHeight;
+
+            if (Movement == MovementType.Up)
+            {
+                calculatedHeight = footerHeight + detailHeight;
+            }
+            else
+            {
+                calculatedHeight = footerHeight - detailHeight;
+            }
+
+            return calculatedHeight.ToString("0.00");
+        }
+
+
+
+        #endregion
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            SetValues();
+            uxHeaderHeightSug.Text = calculateHeaderHeight(headerHeightOG, detailHeightOG, desiredMovement);
+            uxPageFooterHeightSug.Text = calculateHeaderHeight(footerHeightOG, detailHeightOG, desiredMovement);
+
+        }
+
+
+
+
+    }
+}
